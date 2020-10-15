3a6f373dc7d081bea61813fbb38587f5e24df684
nicolas <nicolascaulier@gmail.com>
2020-10-15 13:53:00 +0200

    ok

--- siteperso/bodyaccueil.php
+++ siteperso/bodyaccueil.php
@@ -11,28 +11,28 @@ $str .=  "<div class=\"col-sm-4\">";
 $str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "<div class=\"col-sm-4\">";
-$str .=  "<img src=\"Images/Images_Charte_Graphique/logo_ozinor_propo_SIMPLE_v2.png\" width=150px;>";
+$str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "<div class=\"col-sm-4\">";
-$str .=  "<img src=\"Images/Images_Charte_Graphique/logo_ozinor_propo_SIMPLE_v2.png\" width=150px;>";
+$str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "<div class=\"col-sm-4\">";
-$str .=  "<img src=\"Images/Images_Charte_Graphique/logo_ozinor_propo_SIMPLE_v2.png\" width=150px;>";
+$str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "<div class=\"col-sm-4\">";
-$str .=  "<img src=\"Images/Images_Charte_Graphique/logo_ozinor_propo_SIMPLE_v2.png\" width=150px;>";
+$str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "<div class=\"col-sm-4\">";
-$str .=  "<img src=\"Images/Images_Charte_Graphique/logo_ozinor_propo_SIMPLE_v2.png\" width=150px;>";
+$str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "<div class=\"col-sm-4\">";
-$str .=  "<img src=\"Images/Images_Charte_Graphique/logo_ozinor_propo_SIMPLE_v2.png\" width=150px;>";
+$str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "<div class=\"col-sm-4\">";
-$str .=  "<img src=\"Images/Images_Charte_Graphique/logo_ozinor_propo_SIMPLE_v2.png\" width=150px;>";
+$str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "<div class=\"col-sm-4\">";
-$str .=  "<img src=\"Images/Images_Charte_Graphique/logo_ozinor_propo_SIMPLE_v2.png\" width=150px;>";
+$str .=  "<img src=\"test.jpg\" width=150px;>";
 $str .=  "</div>";
 $str .=  "</div>";
 $str .=  "</div>";
@@ -45,32 +45,35 @@ return $str;
 function carousel()
 {
 
-    $str   = "<div id=\"carouselExampleIndicators\" class=\"carousel slide\" data-ride=\"carousel\">";
-    $str  .= "<ol class=\"carousel-indicators\">";
-    $str  .= "<li data-target=\"#carouselExampleIndicators\" data-slide-to=\"0\" class=\"active\"></li>";
-    $str  .= "<li data-target=\"#carouselExampleIndicators\" data-slide-to=\"1\"></li>";
-    $str  .= "<li data-target=\"#carouselExampleIndicators\" data-slide-to=\"2\"></li>";
-    $str  .= "</ol>";
-    $str  .= "<div class=\"carousel-inner\">";
-    $str  .= "<div class=\"carousel-item active\">";
-    $str  .= "<img class=\"d-block w-100\" src=\"test.jpg\" alt=\"First slide\">";
-    $str  .= "</div>";
-    $str  .= "<div class=\"carousel-item\">";
-    $str  .= "<img class=\"d-block w-100\" src=\"...\" alt=\"Second slide\">";
-    $str  .= "</div>";
-    $str  .= "<div class=\"carousel-item\">";
-    $str  .= "<img class=\"d-block w-100\" src=\"...\" alt=\"Third slide\">";
-    $str  .= "</div>";
-    $str  .= "</div>";
-    $str  .= "<a class=\"carousel-control-prev\" href=\"#carouselExampleIndicators\" role=\"button\" data-slide=\"prev\">";
-    $str  .= "<span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>";
-    $str  .= "<span class=\"sr-only\">Previous</span>";
-    $str  .= "</a>";
-    $str  .= "<a class=\"carousel-control-next\" href=\"#carouselExampleIndicators\" role=\"button\" data-slide=\"next\">";
-    $str  .= "<span class=\"carousel-control-next-icon\" aria-hidden=\"true\"></span>";
-    $str  .= "<span class=\"sr-only\">Next</span>";
-    $str  .= "</a>";
-    $str  .= "</div>";
+    $str = <<<AZE
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img class="d-block w-100" src="test.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="test.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+    <img class="d-block w-100" src="test.jpg" alt="Third slide">
+    </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+    </a>
+    </div>
+
+    AZE;
 
     return $str;
 
